76d62a58585a5fbb0c3839801a920fe4c6efc916
ben <ben@surgison.net>
2019-11-29 18:29:31 +0000

    Clean up photos utility

--- source/plugins/aws-photos.js
+++ source/plugins/aws-photos.js
@@ -2,11 +2,11 @@ const Boom = require('@hapi/boom')
 const { Photos } = require('defra-hapi-utils')
 
 const register = async function (server, options = {}) {
-  const { path = '/photos', alternativeSizes = [], region, apiVersion, bucket, tags = [], enabled = true } = options
+  const { path = '/photos', originalType = 'original', alternativeSizes = [], region, apiVersion, bucket, tags = [], enabled = true } = options
 
   const sizesAllowed = alternativeSizes.map(({ type: size }) => size)
 
-  const photos = new Photos({ region, apiVersion, bucket, alternativeSizes, enabled })
+  const photos = new Photos({ region, apiVersion, bucket, originalType, alternativeSizes, enabled })
 
   const handler = async (request, h) => {
     const filename = encodeURIComponent(request.params.filename)
